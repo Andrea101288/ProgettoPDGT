e174c223026c467a22a9c2cdbd36a7be490732dc
Andrea Mancini <andrea101288@hotmail.it>
2018-04-16 23:19:58 +0200

    1 commit pagina iniziale sito

---
+++ PiattaSisma/index.php
@@ -0,0 +1,21 @@
+
+
+<html>
+<head>
+    <title>pagina iniziale</title>
+</head>
+<body style="background-color:lightgray">
+<body background="a.png" align="center" >
+
+	
+	<h1><i align="center"> <font color="white">PIATTASISMA</font> </i> </h1>
+	<h1><i align="center"> <font color="white">la piattaforma per dati sismici</font> </i> </h1>
+	<h3><i align="center"> <font color="white">accedi al tuo account o registrati e inizia la tua ricerca!</font> </i> </h3>
+	
+	
+	
+	<p align="center"><a href="login.php"> <font color="white"> login <font> <a> | <a href="registrazione.php"> <font color="white">register</font> <a/> </p>	
+	
+
+</body>
+</html>
