b207c34bf6d46554e3eebacb3928c6078af5c6ab
Radeox <dawid.weglarz95@gmail.com>
2018-04-22 15:23:27 +0200

    Implementata possibilità di impostare un periodo nella richiesta #1

--- server/api/views.py
+++ server/api/views.py
@@ -1,5 +1,6 @@
 import urllib
 from datetime import datetime, timedelta
+from dateutil.parser import parse
 from django.views import generic
 from django.http import JsonResponse
 # from .models import Damage, User, TelegramUser
@@ -8,7 +9,7 @@ from .country.all import COUNTRY_LIST
 
 class Earthquakes(generic.View):
     # This value is used to determinate the default period to search
-    default_days_delta = 31
+    default_days_delta = 7
 
     def get(self, request, country):
         # Check if reqeusted country in implemented
@@ -16,17 +17,25 @@ class Earthquakes(generic.View):
             # Get requested country data
             req_country = COUNTRY_LIST[country]()
 
-            # Get current date
-            today = datetime.now()
+            if request.GET.get('endtime') is None:
+                # Get current date
+                to_day = datetime.now()
+            else:
+                # Set the date request by user
+                to_day = parse(request.GET.get('endtime'))
 
-            # Get delta date
-            the_other_day = today - timedelta(days=self.default_days_delta)
+            if request.GET.get('starttime') is None:
+                # Get delta date
+                from_day = to_day - timedelta(days=self.default_days_delta)
+            else:
+                # Set the date request by user
+                from_day = parse(request.GET.get('starttime'))
 
             # URL encode
-            today = urllib.parse.quote_plus(today.isoformat())
-            the_other_day = urllib.parse.quote_plus(the_other_day.isoformat())
+            to_day = urllib.parse.quote_plus(to_day.isoformat())
+            from_day = urllib.parse.quote_plus(from_day.isoformat())
 
             # Request and parse in JSON
-            rv = req_country.return_json(the_other_day, today)
+            rv = req_country.return_json(from_day, to_day)
 
         return JsonResponse(rv)
