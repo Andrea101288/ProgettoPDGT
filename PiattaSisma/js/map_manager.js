ea7d76428a88fef34b2d2fd621881b8d6f7957dc
Radeox <dawid.weglarz95@gmail.com>
2018-04-22 11:49:17 +0200

    Aggiunte info sui marker e cambiata icona terremoto

--- PiattaSisma/js/map_manager.js
+++ PiattaSisma/js/map_manager.js
@@ -1,24 +1,46 @@
 var map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: new google.maps.LatLng(42.39, 12.75),
-        zoom: 2,
-    }); 
-    map.setOptions({ maxZoom: null, minZoom: 1.5,});
-    // TODO: Get this dynamically from page
-    var url = 'http://localhost:8000/terremoti/italy';
-    fetch(url)
-  . then(res => res.json())
-  . then((out) => {
-        out['events'].forEach(function(event) {
-            var latLng = new google.maps.LatLng(event.latitude, event.longitude);
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map
-                // TODO: Make them clickableeee (and smaller is possible)
-            });
-        })
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: new google.maps.LatLng(42.39, 12.75),
+    zoom: 2,
+    minZoom: 3,
+  });
+
+  // TODO: Get this dynamically from page
+  var url = 'http://localhost:8000/terremoti/italy';
+
+  // Richiede il JSON al server
+  fetch(url)
+  .then(res => res.json())
+  .then((out) => {
+    // Scorro gli eventi
+    out['events'].forEach(function(event) {
+      // Imposto le coordinate
+      var latLng = new google.maps.LatLng(event.latitude, event.longitude);
+
+      // Aggiungo il testo del marker
+      var infowindow = new google.maps.InfoWindow({
+        content: '<div class="marker_textbox">' +
+                 '<p><b>Luogo:</b> ' + event.description + '</p>' +
+                 '<p><b>Magnitudine:</b> ' + event.magnitude + '</p>' +
+                 '<p><b>Profondità:</b> ' + (event.depth/1000) + 'km</p>' +
+                 '<p><b>Data:</b> ' + Date(event.time) + '</p>' +
+                 '</div>'
+      });
+
+      // Creo il marker
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        icon: "img/earthquake0.png",
+      });
+
+      // Aggiungo l'evento che visualizzia il testo
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
     })
-    .catch(err => { throw err });
+  })
+  .catch(err => { throw err });
 }
