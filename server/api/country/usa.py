b12f4bab6089dc57ed72a8bd8cbbd62475dad037
Radeox <dawid.weglarz95@gmail.com>
2018-04-23 14:04:32 +0200

    Port a GeoJSON

--- server/api/country/usa.py
+++ server/api/country/usa.py
@@ -33,8 +33,11 @@ class Usa(BasicCountry):
             # Get last update_time
             rv['updated'] = e[0][-1][0].text
 
+            # Set data type
+            rv['type'] = 'FeatureCollection'
+
             # Init events array in JSON
-            rv['events'] = []
+            rv['features'] = []
 
             # Loop on events
             for event in e[0]:
@@ -42,33 +45,41 @@ class Usa(BasicCountry):
                     # Init temporary object
                     tmp = {}
 
+                    # Set data type
+                    tmp['type'] = 'Feature'
+
+                    # Init more stuff
+                    tmp['properties'] = {}
+                    tmp['geometry'] = {}
+                    tmp['geometry'].update({'type': 'Point'})
+                    tmp['geometry']['coordinates'] = []
+
                     # Get event ID
                     event_id = event.attrib['{http://anss.org/xmlns/catalog/0.1}eventid']
-                    tmp.update({'event_id': event_id})
+                    tmp.update({'id': event_id})
 
                     for field in event:
                         # Get description
                         if field.tag == "{http://quakeml.org/xmlns/bed/1.2}description":
-                            tmp.update({'description': field[1].text})
+                            tmp['properties'].update({'description': field[1].text})
                         # Get information from origin
                         elif field.tag == "{http://quakeml.org/xmlns/bed/1.2}origin":
                             for field2 in field:
                                 if field2.tag == "{http://quakeml.org/xmlns/bed/1.2}time":
-                                    tmp.update({'time': field2[0].text})
-                                elif field2.tag == "{http://quakeml.org/xmlns/bed/1.2}latitude":
-                                    tmp.update({'latitude': field2[0].text})
-                                elif field2.tag == "{http://quakeml.org/xmlns/bed/1.2}longitude":
-                                    tmp.update({'longitude': field2[0].text})
+                                    tmp['properties'].update({'time': field2[0].text})
+                                elif field2.tag == "{http://quakeml.org/xmlns/bed/1.2}latitude" or\
+                                        field2.tag == "{http://quakeml.org/xmlns/bed/1.2}longitude":
+                                    tmp['geometry']['coordinates'].append(float(field2[0].text))
                                 elif field2.tag == "{http://quakeml.org/xmlns/bed/1.2}depth":
-                                    tmp.update({'depth': field2[0].text})
+                                    tmp['properties'].update({'depth': field2[0].text})
                         # Get magnitude
                         elif field.tag == "{http://quakeml.org/xmlns/bed/1.2}magnitude":
                             for field2 in field:
                                 if field2.tag == "{http://quakeml.org/xmlns/bed/1.2}mag":
-                                    tmp.update({'magnitude': field2[0].text})
+                                    tmp['properties'].update({'magnitude': field2[0].text})
 
                     # Append the new event
-                    rv['events'].append(tmp)
+                    rv['features'].append(tmp)
 
         # Return final JSON
         return rv
