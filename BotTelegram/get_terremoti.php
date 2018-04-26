fbacbaf62c74583886b54c1a04a2746b96488212
Andrea Mancini <andrea101288@hotmail.it>
2018-04-26 17:10:30 +0200

    ultimazione acquisizione dati e invio all'utente sul Bot

--- BotTelegram/get_terremoti.php
+++ BotTelegram/get_terremoti.php
@@ -14,8 +14,15 @@ function getTerremoti($dati, $token){
         $long = $dati_terremoto->features[$i]->geometry->coordinates[0];
         
         http_request("https://api.telegram.org/bot{$token}/sendLocation?chat_id=".$chat_id."&longitude=".$long."&latitude=".$lat."");    
-        $dati_da_inviare = json_encode($dati_terremoto->features[$i]);
-        http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text= ".urlencode($dati_da_inviare)."");
-    
+        $description1 = json_encode($dati_terremoto->features[$i]->properties->description);
+        $description = explode('"', $description1);
+        $date1 = json_encode($dati_terremoto->features[$i]->properties->time);
+        $date = explode('"', $date1);
+        $time = explode('T', $date[1]);
+        $magnitude1 = json_encode($dati_terremoto->features[$i]->properties->magnitude);
+        $magnitude = explode('"', $magnitude1);
+        $depth1 = json_encode($dati_terremoto->features[$i]->properties->depth);
+        $depth = explode('"', $depth1);
+        http_request("https://api.telegram.org/bot{$token}/sendMessage?chat_id=".$chat_id."&text= Location: ".urlencode($description[1]."\nDate: ".$time[0]."\nTime: ".$time[1]."\nMagnitude: ".$magnitude[1]."\nDepth: ".$depth[1]." mt"));
     }
 }     
